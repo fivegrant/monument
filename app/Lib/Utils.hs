19ca028645a7981e6b835bee32dd04a3774c4953
Five Grant <5@fivegrant.com>
2022-03-22 11:42:29 -0400

    Remove tabs from `monument.cabal`

--- app/Lib/Utils.hs
+++ app/Lib/Utils.hs
@@ -2,7 +2,6 @@ module Lib.Utils where
 
 findRepeat :: Eq a => [a] -> Maybe a
 findRepeat [] = Nothing
-findRepeat (x:[]) = Nothing
+findRepeat [x] = Nothing
 findRepeat (x:xs) | x == head xs = Just x
                   | otherwise = findRepeat $ tail xs
-
